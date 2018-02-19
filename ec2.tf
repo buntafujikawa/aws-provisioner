4c4ff04223db7c606dc20de9add40919411408d1
buntafujikawa <bunta.fujikawa@mkt-apps.com>
2018-02-20 00:13:07 +0900

    add keypair variable

--- ec2.tf
+++ ec2.tf
@@ -1,5 +1,5 @@
 variable "sitename" {}
-variable "ssh_public_key" {}
+variable "key_pair_name" {}
 
 # EC2 production
 resource "aws_instance" "production1" {
@@ -8,7 +8,7 @@ resource "aws_instance" "production1" {
   ebs_optimized               = false
   instance_type               = "t2.micro"
   monitoring                  = true
-  key_name                    = "${aws_key_pair.deployer.key_name}"
+  key_name                    = "${var.key_pair_name}"
   subnet_id                   = "${aws_subnet.public-web-production.id}"
   vpc_security_group_ids      = ["${aws_security_group.app-sg.id}"]
   associate_public_ip_address = true
@@ -29,11 +29,11 @@ resource "aws_instance" "production1" {
 # EC2 staging
 resource "aws_instance" "staging" {
   ami                         = "ami-ceafcba8"
-  availability_zone           = "ap-northeast-1a"
+  availability_zone           = "ap-northeast-1c"
   ebs_optimized               = false
   instance_type               = "t2.micro"
   monitoring                  = true
-  key_name                    = "${aws_key_pair.deployer.key_name}"
+  key_name                    = "${var.key_pair_name}}"
   subnet_id                   = "${aws_subnet.public-web-staging.id}"
   vpc_security_group_ids      = ["${aws_security_group.app-sg.id}"]
   associate_public_ip_address = true
@@ -51,13 +51,7 @@ resource "aws_instance" "staging" {
   }
 }
 
-# keypair
-resource "aws_key_pair" "deployer" {
-  key_name   = "deployer-key"
-  public_key = "${var.ssh_public_key}"
-}
-
-# elastic ip
+# Elastic IP
 resource "aws_eip" "production1-eip" {
   instance = "${aws_instance.production1.id}"
   vpc      = true
@@ -75,3 +69,15 @@ output "elastic_ip_of_production" {
 output "elastic_ip_of_staging" {
   value = "${aws_eip.staging-eip.public_ip}"
 }
+
+# keypair
+/*
+You can create keypair with the code below, but you can not download public key
+> Currently this resource requires an existing user-supplied key pair.
+https://www.terraform.io/docs/providers/aws/r/key_pair.html
+
+resource "aws_key_pair" "deployer" {
+  key_name   = "deployer-key"
+  public_key = "${var.ssh_public_key}"
+}
+*/
